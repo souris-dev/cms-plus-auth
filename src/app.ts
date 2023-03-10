71573930888a5d717982e0721690542bb88ea0d1
Souris Ash <souris_ash@mckinsey.com>
2023-03-10 20:22:09 +0530

    fix: add cors

--- src/app.ts
+++ src/app.ts
@@ -1,7 +1,9 @@
 import express from 'express';
+import cors from 'cors';
 import userRouter from './routes/authRoute.js';
 const app = express();
 
+app.use(cors());
 app.use(express.json());
 
 const port: number = Number(process.env.PORT) || 8002;
